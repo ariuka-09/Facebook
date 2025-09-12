397eab628cc8523746d6b19b4a96bff846ef3bc2
25LP6950 25LP6950 <25LP6950@administrator.local>
2025-09-12 17:39:27 +0800

    like, delete, post buttons activated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -20,163 +20,116 @@ export default function Home() {
       shareCount: "1.6k",
       id: 1,
     },
-    {
-      profilePic: "https://i.pravatar.cc/150?img=12",
-      name: "Tserenbold Bat",
-      date: "3h",
-      setting: "friends",
-      text: "Just finished a 10k run! Feeling amazing 😅",
-      image: "https://picsum.photos/500/300?random=2",
-      reactionCount: "2.1k",
-      commentCount: "120",
-      shareCount: "400",
-      id: 2,
-    },
-    {
-      profilePic: "https://i.pravatar.cc/150?img=23",
-      name: "Nomin Erdene",
-      date: "2d",
-      setting: "public",
-      text: "Loving this new meditation app. Highly recommend! 🧘‍♂️",
-      image: "https://picsum.photos/500/300?random=3",
-      reactionCount: "3.3k",
-      commentCount: "200",
-      shareCount: "700",
-      id: 3,
-    },
-    {
-      profilePic: "https://i.pravatar.cc/150?img=34",
-      name: "Bat-Erdene Munkh",
-      date: "5h",
-      setting: "friends",
-      text: "Check out my latest painting! 🎨",
-      image: "https://picsum.photos/500/300?random=4",
-      reactionCount: "1.2k",
-      commentCount: "80",
-      shareCount: "150",
-      id: 4,
-    },
-    {
-      profilePic: "https://i.pravatar.cc/150?img=41",
-      name: "Enkhtuya Bold",
-      date: "1w",
-      setting: "public",
-      text: "Weekend vibes with friends 🍕🎶",
-      image: "https://picsum.photos/500/300?random=5",
-      reactionCount: "5k",
-      commentCount: "450",
-      shareCount: "900",
-      id: 5,
-    },
+    // {
+    //   profilePic: "https://i.pravatar.cc/150?img=12",
+    //   name: "Tserenbold Bat",
+    //   date: "3h",
+    //   setting: "friends",
+    //   text: "Just finished a 10k run! Feeling amazing 😅",
+    //   image: "https://picsum.photos/500/300?random=2",
+    //   reactionCount: "2.1k",
+    //   commentCount: "120",
+    //   shareCount: "400",
+    //   id: 2,
+    // },
+    // {
+    //   profilePic: "https://i.pravatar.cc/150?img=23",
+    //   name: "Nomin Erdene",
+    //   date: "2d",
+    //   setting: "public",
+    //   text: "Loving this new meditation app. Highly recommend! 🧘‍♂️",
+    //   image: "https://picsum.photos/500/300?random=3",
+    //   reactionCount: "3.3k",
+    //   commentCount: "200",
+    //   shareCount: "700",
+    //   id: 3,
+    // },
+    // {
+    //   profilePic: "https://i.pravatar.cc/150?img=34",
+    //   name: "Bat-Erdene Munkh",
+    //   date: "5h",
+    //   setting: "friends",
+    //   text: "Check out my latest painting! 🎨",
+    //   image: "https://picsum.photos/500/300?random=4",
+    //   reactionCount: "1.2k",
+    //   commentCount: "80",
+    //   shareCount: "150",
+    //   id: 4,
+    // },
+    // {
+    //   profilePic: "https://i.pravatar.cc/150?img=41",
+    //   name: "Enkhtuya Bold",
+    //   date: "1w",
+    //   setting: "public",
+    //   text: "Weekend vibes with friends 🍕🎶",
+    //   image: "https://picsum.photos/500/300?random=5",
+    //   reactionCount: "5k",
+    //   commentCount: "450",
+    //   shareCount: "900",
+    //   id: 5,
+    // },
     // ...repeat up to 100 posts with different names and content
   ];
   const [fbPosts, setFbPosts] = useState(posts);
+  const [isLiked, setIsLiked] = useState(false);
   function removeHandler(id) {
     const filterredPosts = fbPosts.filter((post) => post.id !== id);
     setFbPosts(filterredPosts);
   }
-const post = (name, text) =>{
-  
-}
+  function liked() {
+    setIsLiked(!isLiked);
+  }
+  const post = (name, text, img) => {
+    setFbPosts([
+      ...fbPosts,
+      {
+        profilePic: "https://i.pravatar.cc/150?img=41",
+        name: name,
+        date: "just now",
+        setting: "public",
+        text: text,
+        image: img,
+        reactionCount: "0",
+        commentCount: "0",
+        shareCount: "0",
+        id: fbPosts.length + 1,
+      },
+    ]);
+  };
   return (
+    <div className="w-full h-[100vh] bg-[black] ">
+      <div className="flex flex-col gap-10  ">
+        <div className="flex justify-center">
+          <p className="text-[60px] font-bold text-[#0866ff] ">facebook</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <AddPost post={post}></AddPost>
+        </div>
 
-      <div className="flex flex-col ">
-    <div className="flex flex-col items-center">
-      <AddPost post={post}></AddPost>
-    </div>
- 
-    {fbPosts.map((element, index) => {
-    function post() {}
-    return (
-      <div>
-        <Post
-          id={element.id}
-          key={element.id}
-          profilePic={element.profilePic}
-          name={element.name}
-          date={element.date}
-          setting={element.setting}
-          text={element.text}
-          image={element.image}
-          reactionCount={element.reactionCount}
-          commentCount={element.commentCount}
-          shareCount={element.shareCount}
-          removeHandler={removeHandler}
-        />
-      
+        {fbPosts.map((element, index) => {
+          return (
+            <div>
+              <Post
+                id={element.id}
+                key={element.id}
+                profilePic={element.profilePic}
+                name={element.name}
+                date={element.date}
+                setting={element.setting}
+                text={element.text}
+                image={element.image}
+                reactionCount={element.reactionCount}
+                commentCount={element.commentCount}
+                shareCount={element.shareCount}
+                removeHandler={removeHandler}
+                liked={liked}
+                isLiked={isLiked}
+                setIsLiked={setIsLiked}
+              />
+            </div>
+          );
+        })}
       </div>
-    );
-  })}
-
-     </div>);
+    </div>
+  );
 }
-/* <Post
-        profilePic="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-1/482089205_1170691381723979_607858962561793200_n.jpg?stp=cp0_dst-jpg_s40x40_tt6&_nc_cat=100&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=dxlwkstfRJMQ7kNvwGwbx7Q&_nc_oc=Adl8tSUog2nuAZGfneut2kph-h8SytoLCgk3xV_lzcimlbfRb1gTg9j-0kaDNW46ck4&_nc_zt=24&_nc_ht=scontent.fuln8-1.fna&_nc_gid=gYhsRRyAcZPu6nkA6dw0ZA&oh=00_AfaFMYS-b6UQEc8F19EzpnqUvL8MT-zp8Ukbu5LmVwKcfw&oe=68BE19CC"
-        name="Khandsuren Gantogtokh"
-        date="1d"
-        setting="public"
-        text="best meditation with swimming 🏊‍♀️"
-        image="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/541446937_1328896899236759_4831060987877852604_n.jpg?stp=cp6_dst-jpg_p526x296_tt6&_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=mfYdTwPjJowQ7kNvwG2GUbs&_nc_oc=AdmGOl-exrwz5ctAbye0rtQ-yoEn0p9fJG4GZSlVc_YRVYySng0NZg5F7mYWqma7Qms&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=6lGsn2N8JQI3mYaeaC2Oeg&oh=00_AfaaMGJM7KUSUcCt5-bEZR2eDolEej_aFYaa9XXMhynFww&oe=68BE1B7A"
-        reactionCount="4.6k"
-        commentCount="499"
-        shareCount="1.6k"
-      ></Post> */
-/* <Post
-        profilePic="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-1/351363919_199334103062894_9068609386125173171_n.jpg?stp=cp0_dst-jpg_s40x40_tt6&_nc_cat=1&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=PmuwyLW-OsIQ7kNvwFX8qXB&_nc_oc=AdkXsj3IdvX8G0uzP1bfsOXQYI0HbZlHEHQoAQU6sWPuAWQgy2utSbhTV1-_fnkge5M&_nc_zt=24&_nc_ht=scontent.fuln8-1.fna&_nc_gid=0A_-_psB6lbsZ2H9hpVWzA&oh=00_AfYN3rdmPkTsyzl_Z4Fdkys5NMHOWb9dOidI6s29atauxA&oe=68BE2E3E"
-        name="Meme for mathematicians"
-        date="1d"
-        setting="public"
-        text=""
-        image="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/542187398_1071988768482683_8126962495475436157_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=QAP68tZm4wYQ7kNvwGQvDl2&_nc_oc=AdmcNbd74iygnfwF3SfhhiaVQ41bXwLkxu38uixVmFmmThRzkyrLyANWuLUc0Cixtz8&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=0A_-_psB6lbsZ2H9hpVWzA&oh=00_AfatJ8f3EWgDSBWzbd7rweKeYd--JefRoTt-YDCQGvO9ZQ&oe=68BE20CB"
-        reactionCount="1.9k"
-        commentCount="169"
-        shareCount="183"
-      ></Post>
-      <Post
-        profilePic="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-1/347811925_265887139227330_7415253662892000460_n.jpg?stp=cp0_dst-jpg_s40x40_tt6&_nc_cat=1&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=ZtQrIMf5A0cQ7kNvwEpDEp6&_nc_oc=AdlOTnM7O8G5OKWuMtSvOD0a2TYI2tmPwnZw4q9UYd9SvZ0oLngWCp6xmy8_LK3RGaY&_nc_zt=24&_nc_ht=scontent.fuln8-1.fna&_nc_gid=DBT8iWjrEMpyh-tcruC6Vg&oh=00_Afapkmf_xxthigeSSGL008BoZj1glnfvp0r4HOaNhYaYGw&oe=68BE280C"
-        name="NBA"
-        date="12m"
-        setting="public"
-        text="The two moods of every NBA fan during this offseason. We're getting there! 🥹⏳… See more"
-        image="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/542710457_1328266838664080_7118413287710316908_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=xpMqVfSxRjcQ7kNvwG3Zc-0&_nc_oc=AdnQU3hmx4_usN3M6C_OTWsn9meMM8n5MAahEG5YO7BrexnY7KMvK12NpEfptNVH5kY&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=fyOE-mqBHCJicWDAots6yg&oh=00_Afa3BlFN3nIQLt-Y3iDCPWbVk7meYhX2gxdWtvgFws-lmQ&oe=68BE236B"
-        reactionCount="107"
-        commentCount="1"
-        shareCount="5"
-      ></Post>
-      <Balloon color="red"></Balloon> */
-
-// <div>
-//   <Post
-//     profilePic="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-1/482089205_1170691381723979_607858962561793200_n.jpg?stp=cp0_dst-jpg_s40x40_tt6&_nc_cat=100&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=dxlwkstfRJMQ7kNvwGwbx7Q&_nc_oc=Adl8tSUog2nuAZGfneut2kph-h8SytoLCgk3xV_lzcimlbfRb1gTg9j-0kaDNW46ck4&_nc_zt=24&_nc_ht=scontent.fuln8-1.fna&_nc_gid=gYhsRRyAcZPu6nkA6dw0ZA&oh=00_AfaFMYS-b6UQEc8F19EzpnqUvL8MT-zp8Ukbu5LmVwKcfw&oe=68BE19CC"
-//     name="Khandsuren Gantogtokh"
-//     date="1d"
-//     setting="public"
-//     text="best meditation with swimming 🏊‍♀️"
-//     image="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/541446937_1328896899236759_4831060987877852604_n.jpg?stp=cp6_dst-jpg_p526x296_tt6&_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=mfYdTwPjJowQ7kNvwG2GUbs&_nc_oc=AdmGOl-exrwz5ctAbye0rtQ-yoEn0p9fJG4GZSlVc_YRVYySng0NZg5F7mYWqma7Qms&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=6lGsn2N8JQI3mYaeaC2Oeg&oh=00_AfaaMGJM7KUSUcCt5-bEZR2eDolEej_aFYaa9XXMhynFww&oe=68BE1B7A"
-//     reactionCount="4.6k"
-//     commentCount="499"
-//     shareCount="1.6k"
-//   ></Post>
-//   <Post
-//     profilePic="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-1/351363919_199334103062894_9068609386125173171_n.jpg?stp=cp0_dst-jpg_s40x40_tt6&_nc_cat=1&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=PmuwyLW-OsIQ7kNvwFX8qXB&_nc_oc=AdkXsj3IdvX8G0uzP1bfsOXQYI0HbZlHEHQoAQU6sWPuAWQgy2utSbhTV1-_fnkge5M&_nc_zt=24&_nc_ht=scontent.fuln8-1.fna&_nc_gid=0A_-_psB6lbsZ2H9hpVWzA&oh=00_AfYN3rdmPkTsyzl_Z4Fdkys5NMHOWb9dOidI6s29atauxA&oe=68BE2E3E"
-//     name="Meme for mathematicians"
-//     date="1d"
-//     setting="public"
-//     text=""
-//     image="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/542187398_1071988768482683_8126962495475436157_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=QAP68tZm4wYQ7kNvwGQvDl2&_nc_oc=AdmcNbd74iygnfwF3SfhhiaVQ41bXwLkxu38uixVmFmmThRzkyrLyANWuLUc0Cixtz8&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=0A_-_psB6lbsZ2H9hpVWzA&oh=00_AfatJ8f3EWgDSBWzbd7rweKeYd--JefRoTt-YDCQGvO9ZQ&oe=68BE20CB"
-//     reactionCount="1.9k"
-//     commentCount="169"
-//     shareCount="183"
-//   ></Post>
-//   <Post
-//     profilePic="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-1/347811925_265887139227330_7415253662892000460_n.jpg?stp=cp0_dst-jpg_s40x40_tt6&_nc_cat=1&ccb=1-7&_nc_sid=2d3e12&_nc_ohc=ZtQrIMf5A0cQ7kNvwEpDEp6&_nc_oc=AdlOTnM7O8G5OKWuMtSvOD0a2TYI2tmPwnZw4q9UYd9SvZ0oLngWCp6xmy8_LK3RGaY&_nc_zt=24&_nc_ht=scontent.fuln8-1.fna&_nc_gid=DBT8iWjrEMpyh-tcruC6Vg&oh=00_Afapkmf_xxthigeSSGL008BoZj1glnfvp0r4HOaNhYaYGw&oe=68BE280C"
-//     name="NBA"
-//     date="12m"
-//     setting="public"
-//     text="The two moods of every NBA fan during this offseason. We're getting there! 🥹⏳… See more"
-//     image="https://scontent.fuln8-1.fna.fbcdn.net/v/t39.30808-6/542710457_1328266838664080_7118413287710316908_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_ohc=xpMqVfSxRjcQ7kNvwG3Zc-0&_nc_oc=AdnQU3hmx4_usN3M6C_OTWsn9meMM8n5MAahEG5YO7BrexnY7KMvK12NpEfptNVH5kY&_nc_zt=23&_nc_ht=scontent.fuln8-1.fna&_nc_gid=fyOE-mqBHCJicWDAots6yg&oh=00_Afa3BlFN3nIQLt-Y3iDCPWbVk7meYhX2gxdWtvgFws-lmQ&oe=68BE236B"
-//     reactionCount="107"
-//     commentCount="1"
-//     shareCount="5"
-//   ></Post>
-// </div>
--- src/components/AddPost.jsx
+++ src/components/AddPost.jsx
@@ -3,18 +3,54 @@ import { useState } from "react";
 export function AddPost(props) {
   const [name, setName] = useState("");
   const [text, setText] = useState("");
-  const {post} = props;
-  function handleNameChange(e){
-    setName(e.target.value)
+  const [img, setImg] = useState("");
+  const { post } = props;
+  function handleNameChange(e) {
+    setName(e.target.value);
   }
-  function handleTextChange(e){
-    setText(e.target.value  )
+  function handleTextChange(e) {
+    setText(e.target.value);
+  }
+  function handleImgChange(e) {
+    setImg(e.target.value);
   }
   return (
-    <div className="flex flex-col w-fit gap-4">
-      <input type="text" value={name} onChange={handleNameChange} className="bg-white text-black" />
-      <input type="text" value={text} onChange={handleTextChange} className="bg-[violet] text-black " />
-      <button className="cursor-pointer border-2 border-white" onClick={post(name, text)}> Post</button>
+    <div className="px-10 rounded">
+      <div className="flex flex-col w-fit gap-4">
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={handleNameChange}
+          className="bg-[white] rounded p-1.5 text-[15px] text-black"
+        />
+        <input
+          type="text"
+          placeholder="Text"
+          value={text}
+          onChange={handleTextChange}
+          className="bg-[white] rounded p-1.5 text-[15px] text-black "
+        />
+        <input
+          type="text"
+          placeholder="Img"
+          value={img}
+          onChange={handleImgChange}
+          className="bg-[white] rounded p-1.5 text-[15px] text-black "
+        />
+        <button
+          className="cursor-pointer border-2 border-white hover:opacity-90 bg-[#0866ff]"
+          onClick={() => {
+            post(name, text, img);
+            setName("");
+            setText("");
+            setImg("");
+          }}
+        >
+          {" "}
+          Post
+        </button>
+      </div>
     </div>
   );
 }
